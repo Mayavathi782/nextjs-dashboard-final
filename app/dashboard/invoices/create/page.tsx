fc1a6a8a771ce9fd80154173a3196a1b0aad7fd5
Mayavathi E <mayavath26@gmail.com>
2026-08-25 22:01:38 +0530

    Force dynamic rendering for dashboard pages

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -2,7 +2,7 @@ import { fetchCustomers } from '@/app/lib/data';
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { Metadata } from 'next';
-
+export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: 'Create Invoice',
 };
